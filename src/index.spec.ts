23fd56cd460cb440073273ac5cef6bab33c8ac58
Codermar <jgalfonso@me.com>
2019-02-15 11:08:22 -0800

    Update test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,12 +1,8 @@
-import { name, edit } from './index';
+import { edit } from './index';
 import { expect } from 'chai';
 import 'mocha';
 
 describe('OpenApi Editor', () => {
-
-  it('It should return openapi-editor', () => {
-    expect(name).to.equal('openapi-editor');
-  });
 
   it('It Should expect edit to be function', () => {
     expect(typeof edit).to.equals('function');
